e65597561cedb23e130b3469bed47ec8c229af5e
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-04-22 17:23:40 +0500

    Create song routes

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
+const songRouter = require('./routes/songRoutes')
+
 const app = express();
+
+// Routes
+app.use('/api/v1/songs', songRouter)
 
 module.exports = app;
