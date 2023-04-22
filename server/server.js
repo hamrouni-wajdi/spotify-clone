5f779a18a2fafd6bfc3c33fdecfcf2d6a3e97979
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-04-22 17:58:37 +0500

    Connect to the database

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const chalk = require('chalk')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose');
 
+dotenv.config({path: './.env'})
 const app = require('./app')
 
+// Connect to the database
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD)
+
+mongoose.connect(DB).then(() => {
+  console.log(chalk.hex('#78dce8').bold('DATABASE CONNECTION SUCCESSFUL'))
+})
 
 app.get('/', (req, res) => {
   res.send('Hello, world')
 })
 
-app.listen(3000, () => {
-  console.log(chalk.hex('#78dce8').bold('LISTENING ON PORT 3000'))
+app.listen(process.env.PORT || 3000, () => {
+  console.log(chalk.hex('#78dce8').bold('LISTENING ON PORT ' + process.env.PORT))
 })
