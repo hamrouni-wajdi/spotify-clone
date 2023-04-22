96c2fa4226e7f77f73e9b2e552f0fff84b34904f
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-04-22 18:04:33 +0500

    Create basic song model

---
+++ server/models/songModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const songSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A song must have a name'],
+        trim: true,
+    },
+    // artist: {
+    //     type: String,
+    //     required: [true, 'A song must have a artist'],
+    // },
+    // song: {
+    //     type: String,
+    //     required: [true, 'A song must have a song'],
+    // },
+    img: {
+        type: String,
+        required: [true, 'A song must have a cover img'],
+    },
+    // duration: {
+    //     type: Number,
+    //     required: [true, 'A song must have a duration'],
+    // },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+}, {
+    toJSON: {virtuals: true},
+    toObject: {virtuals: true}
+})
+
+const Song = new mongoose.model('Song', songSchema)
+
+module.exports = Song
