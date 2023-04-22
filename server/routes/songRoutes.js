d2ef070098924a12d2ed32d6d5c9eec5722259d3
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-04-22 20:48:00 +0500

    Create createSong controller and setup multer

--- server/routes/songRoutes.js
+++ server/routes/songRoutes.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const songController = require('../controllers/songController');
 
 const router = express.Router();
+
+router
+  .route('/')
+  .post(songController.uploadSongFile, songController.createSong);
 
 module.exports = router;
