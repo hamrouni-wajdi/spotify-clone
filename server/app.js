b96d75b90d3666ee56c173ef857ead188afbf23a
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-04-24 18:34:38 +0500

    Create global error handler

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const globalErrorHandler = require('./controllers/errorController');
 const songRouter = require('./routes/songRoutes');
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.static('songs'));
 // Routes
 app.use('/api/v1/songs', songRouter);
 
+// Unhandled routes
 app.all('*', (req, res, next) => {
   res.status(404).json({
     status: 'fail',
@@ -19,5 +21,8 @@ app.all('*', (req, res, next) => {
   });
   next();
 });
+
+// Global error handler
+app.use(globalErrorHandler);
 
 module.exports = app;
