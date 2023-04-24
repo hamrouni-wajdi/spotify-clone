08e0112d0780cde987c3bbd1fad59677f79c242d
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-04-24 20:56:43 +0500

    Handle invalid ID errors

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -1,8 +1,55 @@
+const chalk = require('chalk');
+const AppError = require('../utils/appError');
+
+const handleCastError = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+// Error sender function
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak details to client
+  } else {
+    // Log error
+    console.log('ERROR 💥', err);
+    chalk.hex('#ff6188').bold('ERROR 💥', err);
+
+    // Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development ') sendErrorDev(err, res);
+  if (process.env.NODE_ENV === 'production ') {
+    let error = Object.assign(err);
+
+    if (error.name === 'CastError') error = handleCastError(error);
+
+    sendErrorProd(error, res);
+  }
+
+  next();
 };
