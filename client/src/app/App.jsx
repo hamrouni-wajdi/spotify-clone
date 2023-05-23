0a1b9b2e988b7101f0bd461dca739f9e82233c20
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-05-23 21:54:45 +0500

    🎨 Design main nav

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,7 +1,22 @@
 import "./App.scss";
 
+import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
+
 const App = (props) => {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <div className="app-nav">
+        <div className="app-nav__history">
+          <div className="app-nav__history-icon">
+            <IoChevronBackOutline />
+          </div>
+          <div className="app-nav__history-icon">
+            <IoChevronForwardOutline />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
