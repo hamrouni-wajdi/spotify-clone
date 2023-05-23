94d3d0f24fb613a00a768113e1a5106b1e209d70
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-05-23 20:55:31 +0500

    🎨 Design side nav

--- client/src/nav/Nav.jsx
+++ client/src/nav/Nav.jsx
@@ -1,7 +1,17 @@
 import "./Nav.scss";
+import { IoHomeOutline, IoSearch } from "react-icons/io5";
 
 const Nav = (props) => {
-  return <div className="nav">Nav</div>;
+  return (
+    <div className="nav">
+      <div className="nav-link">
+        <IoHomeOutline /> Home
+      </div>
+      <div className="nav-link">
+        <IoSearch /> Search
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
