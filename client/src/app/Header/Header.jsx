e54a396c3ca28097d7d6464c92e44979e628788b
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-05-26 10:02:14 +0500

    🎨 Add latest played artist list to header

--- client/src/app/Header/Header.jsx
+++ client/src/app/Header/Header.jsx
@@ -1,5 +1,7 @@
 import './Header.scss';
 
+import { IoPlayCircle } from 'react-icons/io5';
+
 const img =
   'https://images.unsplash.com/photo-1684654488308-2229de99e7a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80';
 
@@ -13,6 +15,10 @@ const Header = (props) => {
         {latest.map((el) => (
           <div className='app-header__latest-card'>
             <img src={img} />
+            <div className='app-header__latest-card-name'>
+              <span>Artist Name</span>
+              <IoPlayCircle className='app-header__latest-card-btn' />
+            </div>
           </div>
         ))}
       </div>
