085590f1bf45414faa7293a68f1e4cb29a5b1061
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-05-27 21:22:20 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -21,7 +21,12 @@ const app = express();
 // );
 // app.options('*', cors());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
 app.use(helmet());
 app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
