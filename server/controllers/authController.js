235ff76128e64587f55ebba3e5917b52572df8e0
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-05-27 19:03:24 +0500

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -24,6 +24,7 @@ const createSendToken = (user, statusCode, res) => {
   // Generate cookie
   const cookieOptions = {
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
   };
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
   res.cookie('jwt', token, cookieOptions);
