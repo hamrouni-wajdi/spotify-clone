913101fdf78c82eb49354980b262c8f9579b06c3
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-05-28 11:24:23 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -23,7 +23,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin:
+      'http://localhost:3000,https://spotify-clone-maqsudtolipov.vercel.app/',
     credentials: true,
   })
 );
