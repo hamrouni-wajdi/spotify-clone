cc3545b4229d5f50400cc942034be48a9da67a40
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-05-28 11:03:03 +0500

    🍪 set sameSite none

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -25,6 +25,7 @@ const createSendToken = (user, statusCode, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    sameSite: 'none',
   };
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
   res.cookie('jwt', token, cookieOptions);
