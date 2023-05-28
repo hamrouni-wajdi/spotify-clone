f0cba8383d49838cf09cb2638db1341cf2201bdb
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-05-28 14:05:57 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -23,7 +23,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://maqsud-spotify.vercel.app',
+    // origin: 'https://maqsud-spotify.vercel.app',
+    origin: 'http://localhost:3000',
     credentials: true,
   })
 );
