8414437366273abb867ca37c3fd9ca2f6498fd67
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-05-28 11:28:59 +0500

    🍪 turn off secure cookie

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(
   cors({
     origin:
-      'http://localhost:3000,https://spotify-clone-maqsudtolipov.vercel.app/',
+      'http://localhost:3000',
     credentials: true,
   })
 );
