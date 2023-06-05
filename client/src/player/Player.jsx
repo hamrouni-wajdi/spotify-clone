a18c95861a42fb1c72f2b78bb6d4ae79526e865c
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-06-05 19:00:41 +0500

    🎨 Show song data in player

--- client/src/player/Player.jsx
+++ client/src/player/Player.jsx
@@ -1,29 +1,34 @@
 import './Player.scss';
 import { IoHeart } from 'react-icons/io5';
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {getSong} from "../store/thunks/song";
 
 const img =
   'https://images.unsplash.com/photo-1659922964423-bbecb913f7ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80';
 
 const Player = (props) => {
+  const {song} = useSelector(state => state.song)
   const dispatch = useDispatch()
 
   const getSongHandler = () => {
-    console.log('get song')
-    dispatch(getSong({id: '647ddc919302dce8e676f767'}))
+    dispatch(getSong({id: '647dea0845407a3b165ea4d3'}))
   }
 
   return (
     <div className='player'>
+      {song &&
+        <>
       <div className='player-song'>
-        <img src={img} alt='' />
+        <img src={song.img} alt='' />
         <div className='player-song__context'>
-          <span className='player-song__name'>Porsche</span>
-          <span className='player-song__artist'>Racers</span>
+          <span className='player-song__name'>{song.name}</span>
+          <span className='player-song__artist'>{song.artist.name}</span>
         </div>
         <IoHeart className='player-song__like' />
       </div>
+
+         </>
+      }
       <div>
         <button onClick={getSongHandler}>aa</button>
       </div>
