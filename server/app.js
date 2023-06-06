3b9a59be01cbeeb299207242134269cdbbffe540
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-06-07 03:32:56 +0500

    Revert "🚀 Try deployed server"
    This reverts commit 2596e770e2138d5628c4a03afe61b7ce34e57130.

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(
   cors({
     // origin: 'https://maqsud-spotify.vercel.app',
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3000',
     credentials: true,
   })
 );
