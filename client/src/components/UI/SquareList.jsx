cdf3f94ada02e9d08b78d2e772cb72aba45b3311
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-09-13 21:14:11 +0500

    🛠️ Fix navigation links

--- client/src/components/UI/SquareList.jsx
+++ client/src/components/UI/SquareList.jsx
@@ -1,14 +1,18 @@
 import "./SquareList.scss";
+import { Link } from "react-router-dom";
 
 const squareList = ({ list, artist }) => {
   return (
     <div className="square-list">
       {list.map((el) => (
-        <div className={"square-card " + (artist && "square-card--artist")}>
+        <Link
+          to={artist ? `/artist/${el.id}` : `/playlist/${el.id}`}
+          className={"square-card " + (artist && "square-card--artist")}
+        >
           <img src={el.img} alt={el.name} />
           <div className="square-card__name">{el.name}</div>
           {artist ? <div>Artist</div> : <div>By {el.user.name}</div>}
-        </div>
+        </Link>
       ))}
     </div>
   );
--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -17,14 +17,14 @@ const Nav = (props) => {
   return (
     <div>
       <div className="nav">
-        <div className="nav-link">
+        <Link to={"/"} className="nav-link">
           <IoHomeOutline />
           <span>Home</span>
-        </div>
-        <div className="nav-link">
+        </Link>
+        <Link to="/search" className="nav-link">
           <IoSearch />
           <span>Search</span>
-        </div>
+        </Link>
       </div>
       <div className="library">
         <div className="library-link">
@@ -36,24 +36,11 @@ const Nav = (props) => {
         </div>
       </div>
       <div className="nav">
-        <div className="nav-link">
+        <Link to="/" className="nav-link">
           <IoPersonCircleOutline />
-          <Link className="nav-link" to="/profile">
-            Profile
-          </Link>
-        </div>
+          <span>Profile</span>
+        </Link>
       </div>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/search"}>Search</Link>
-      <br />
-      <Link to={"/artist/64f9832d10dbf93f2e5af3cc"}>AW</Link>
-      <Link to={"/artist/64f88ed5c20246c3c14e688d"}>CP</Link>
-      <Link to={"/artist/64f9e78083e383c2f5e11c3e"}>CPY</Link>
-      <br />
-      <Link to={"/playlist/64fc4ad1cf33fdb3633e6bb9"}>P1</Link>
-      <Link to={"/playlist/6501b805d0592bee92c1e15e"}>P2</Link>
-      {/*<Link to={"/artist/64f88ed5c20246c3c14e688d"}>CP</Link>*/}
-      {/*<Link to={"/artist/64f9e78083e383c2f5e11c3e"}>CPY</Link>*/}
     </div>
   );
 };
