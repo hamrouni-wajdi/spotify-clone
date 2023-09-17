f377f4cbad80270be471e03aece53e5c45d8afc9
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-09-17 16:32:23 +0500

    🐳 Change axios url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://spotify-api.maqsud.me/api/v1/",
-  // baseURL: "http://localhost:8000/api/v1/",
-  baseURL: "https://spotify-tl14.onrender.com/api/v1/",
+  baseURL: "https://spotify-api.maqsud.me/api/v1/",
+  // baseURL: "http://localhost:8000/api/v1/"
   withCredentials: true,
 });
