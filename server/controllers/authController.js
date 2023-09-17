2bf2e8410bd6249297d6b666c629ae7302b98b62
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-09-17 16:18:16 +0500

    🧪 Test cookies on server

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -25,7 +25,7 @@ const createSendToken = (user, statusCode, req, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    // sameSite: 'none',
+    sameSite: 'none',
     secure: true,
   };
   // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
