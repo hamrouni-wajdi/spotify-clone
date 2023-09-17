7d391bd2029cc576eace3741b3aa64e5789b2236
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-09-17 16:28:07 +0500

    🪸 Test multiple cors

--- server/app.js
+++ server/app.js
@@ -16,19 +16,9 @@ const app = express();
 
 app.enable('trust proxy');
 
-// Middlewares
-// app.use(cors());
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//   })
-// );
-// app.options('*', cors());
-
 app.use(
   cors({
-    // origin: 'https://maqsud-spotify.vercel.app',
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://maqsud-spotify.vercel.app'],
     credentials: true,
   })
 );
