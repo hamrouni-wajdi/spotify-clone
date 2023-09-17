1beb1d6939b734242a670c36a58c6ccf4d31a9df
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-09-17 16:49:30 +0500

    🪸 Set sameSite lax

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -25,9 +25,9 @@ const createSendToken = (user, statusCode, req, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: 'none',
+    sameSite: 'lax',
+    secure: true,
   };
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
   res.cookie('jwt', token, cookieOptions);
 
   user.photo = `${req.protocol}://${req.get('host')}/public/users/${
