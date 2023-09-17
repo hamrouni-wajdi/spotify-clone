91d2e252baa5cd0443d0dd21484c2f486ffb91aa
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-09-17 16:59:21 +0500

    🪸 Set sameSite

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -25,7 +25,6 @@ const createSendToken = (user, statusCode, req, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: 'strict',
     secure: true,
     withCredentials: true,
   };
