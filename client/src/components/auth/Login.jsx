5332714188b9a9a04b861de90841651b4f48e880
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-09-24 00:44:49 +0500

    🎨 Update auth form design

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -23,23 +23,16 @@ const Login = (props) => {
         <div className="auth">
           <form className="auth-form" onSubmit={login}>
             <img className="auth-form__logo" src={logo} alt="" />
-            <Link to="/forgotPassword" className="auth-form__link">
-              Forgot password?
-            </Link>
+
             <Link to="/signup" className="auth-form__link">
               Sign Up here
             </Link>
             <input type="text" placeholder="Email" required />
             <input type="text" placeholder="Password" required />
+            <Link to="/forgotPassword" className="auth-form__link">
+              Forgot password?
+            </Link>
             <button type="submit">Log In</button>
-            {/*{sucess && (*/}
-            {/*  <p>*/}
-            {/*    You are sucessfully logged in{' '}*/}
-            {/*    <Link to='/' className='auth-form__link'>*/}
-            {/*      Go to Home*/}
-            {/*    </Link>*/}
-            {/*  </p>*/}
-            {/*)}*/}
           </form>
         </div>
       ) : (
