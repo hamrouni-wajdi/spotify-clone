bd1a5bd0e952fb36cd6178b2e549f51d0e32918d
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-09-27 02:02:13 +0500

    🎛️ Auto switch server between envs

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://spotify-api.maqsud.me/api/v1/",
-  baseURL: "http://localhost:8000/api/v1/",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8000/api/v1/"
+      : "https://spotify-api.maqsud.me/api/v1/",
   withCredentials: true,
 });
