4d3a76db688ddf58b635e956ed05f7c3d11d2190
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-09-30 11:52:51 +0500

    ♻️ Refactor css: app component

--- client/src/components/app/App.jsx
+++ client/src/components/app/App.jsx
@@ -8,21 +8,21 @@ const App = (props) => {
 
   return (
     <div className="app">
-      <div className="app-nav">
-        <div className="app-nav__history">
-          <div className="app-nav__history-icon">
+      <div className="app__nav">
+        <div className="app__nav__history">
+          <div className="app__nav__history-icon">
             <IoChevronBackOutline />
           </div>
-          <div className="app-nav__history-icon">
+          <div className="app__nav__history-icon">
             <IoChevronForwardOutline />
           </div>
         </div>
-        <div className="app-nav__profile">
+        <div className="app__nav__profile">
           <img
             crossOrigin="anonymous"
             src={data.img}
             alt=""
-            className="app-nav__profile--img"
+            className="app__nav__profile--img"
           />
         </div>
       </div>
