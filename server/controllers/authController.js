2e893df6de50493de042f41a4c57e764bcc20248
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-09-30 21:55:55 +0500

    🔐 Fix logout

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -30,7 +30,7 @@ const createSendToken = (user, statusCode, req, res) => {
 
   res.status(statusCode).json({
     status: 'success',
-    token,
+    // token,
     data: { user },
   });
 };
@@ -83,10 +83,14 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 exports.logout = catchAsync(async (req, res, next) => {
-  res.cookie('jwt', 'loggedOut', {
-    expires: new Date(Date.now() - 60 * 10 * 1000),
+  const cookieOptions = {
+    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-  });
+    sameSite: 'none',
+    secure: true,
+    withCredentials: true,
+  };
+  res.cookie('jwt', 'loggedOut', cookieOptions);
   res.status(200).json({ status: 'success', message: '✌️ See you soon!' });
 });
 
