35111eef227e6f50c676f39c94051a3bb3936d82
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-12-12 22:12:57 +0500

    🚀 Implement navigation history

--- client/src/components/app/App.jsx
+++ client/src/components/app/App.jsx
@@ -2,19 +2,21 @@ import "./App.scss";
 
 import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const App = (props) => {
   const { data } = useSelector((state) => state.user);
+  const navigate = useNavigate();
 
   return (
     <div className="app">
       <div className="app__nav">
         <div className="app__nav__history">
           <div className="app__nav__history-icon">
-            <IoChevronBackOutline />
+            <IoChevronBackOutline onClick={() => navigate(-1)} />
           </div>
           <div className="app__nav__history-icon">
-            <IoChevronForwardOutline />
+            <IoChevronForwardOutline onClick={() => navigate(1)} />
           </div>
         </div>
         <div className="app__nav__profile">
