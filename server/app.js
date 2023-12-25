d803bfdc9cc908bb046057a4fa100cb24b083c5b
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-12-25 14:44:03 +0500

    ⚡ Migrate to Vite

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === 'development'
-        ? 'http://localhost:3000'
+        ? 'http://localhost:5173'
         : 'https://maqsud-spotify.vercel.app',
     credentials: true,
   })
