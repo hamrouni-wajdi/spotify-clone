a6b5ef711360f39675283a2fbb84366b754f003f
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2024-01-22 17:20:16 +0500

    🗑️ Remove dev previews

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,19 +5,12 @@ import "./index.scss";
 
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { DevSupport } from "@react-buddy/ide-toolbox";
-import { ComponentPreviews, useInitial } from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <DevSupport
-        ComponentPreviews={ComponentPreviews}
-        useInitialHook={useInitial}
-      >
-        <MainApp />
-      </DevSupport>
+      <MainApp />
     </Provider>
   </React.StrictMode>,
 );
