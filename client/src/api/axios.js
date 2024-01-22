be716f616141f1c8da28649beafe0f0fa0603d20
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2024-01-22 18:07:46 +0500

    ☄️ Update server URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:8000/api/v1/"
-      : "https://spotify-tl14.onrender.com/",
+      : "https://spotify-tl14.onrender.com/api/v1/",
   withCredentials: true,
 });
