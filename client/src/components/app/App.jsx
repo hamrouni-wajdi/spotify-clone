2d81cf656b2e8c404878d6d0502539f3e3c1256a
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2024-05-09 23:24:58 +0500

    🧹 Refactor App into styled component

--- client/src/components/app/App.jsx
+++ client/src/components/app/App.jsx
@@ -1,13 +1,25 @@
-import "./App.scss";
 import Nav from "../../layout/Nav/Nav.jsx";
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  height: 100%;
+  margin-top: 0.8rem;
+  margin-right: 0.8rem;
+
+  position: relative;
+  overflow-y: scroll;
+
+  background-color: var(--color-black);
+  border-radius: 8px;
+`;
 
 const App = ({ children }) => {
   return (
-    <div className="app">
+    <StyledApp>
       <Nav />
 
       {children}
-    </div>
+    </StyledApp>
   );
 };
 
