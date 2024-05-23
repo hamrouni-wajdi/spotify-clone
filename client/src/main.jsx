42379e9b75c4dbfeb2c8fdb9180d0600e82d5827
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2024-05-23 21:34:13 +0500

    🗑️ Remove Maintainance page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRoutes from './AppRoutes.jsx';
-import Maintenance from './pages/Maintenance.jsx';
 import { Provider } from 'react-redux';
 import { store } from './app/store.js';
 import GlobalStyles from './styles/globalStyles.js';
 import './styles.scss';
 
-const isMaintenance = import.meta.env.VITE_MAINTENANCE;
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {isMaintenance ? (
-      <Maintenance />
-    ) : (
-      <Provider store={store}>
-        <AppRoutes />
-      </Provider>
-    )}
+    <Provider store={store}>
+      <AppRoutes />
+    </Provider>
 
     <GlobalStyles />
   </React.StrictMode>,
